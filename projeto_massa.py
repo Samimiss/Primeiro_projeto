d31938786083972c2c4840c668ed0692cd4553f1
Samimiss <samily64884686@edu.df.senac.br>
2026-06-12 21:25:50 -0300

    Aprendendo variáveis e imprimir na tela

---
+++ projeto_massa.py
@@ -0,0 +1,10 @@
+nome = "Samily"        # str- string
+sobrenome = "Campos"   # str- string
+idade = 20             # int- inteiro
+altura = 1.70          # flt- float
+ano_nascimento = 2006  # int- inteiro
+maior_de_idade = True  # bool- booleano : True;false
+
+
+print("Olá", nome, sobrenome, "Você tem", idade,  " sua altura é ", altura, "Você é maior de idade?", maior_de_idade)
+print(f"Olá, {nome} {sobrenome},  você tem {20} anos, nasceu em {ano_nascimento},  sua altura é {altura} , e você é maior de idade? {maior_de_idade}!!")
