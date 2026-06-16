3499de2b142a3242d8841099842ab07fb6a1503c
Samimiss <samilyyohana@gmail.com>
2026-06-16 20:34:21 -0300

    aula de estrutura condicional

---
+++ Estrutura_condicional.py
@@ -0,0 +1,19 @@
+# Casar ou comprar guma bike?
+
+print("Casar ou Comprar uma bike?")
+resposta = input("Você está gorda? s/n ")
+if resposta == "s":
+    quer_emagrecer = input("Você quer emagrecer? ")
+    if quer_emagrecer == "s":
+        print("Compre uma bicicleta!!!")
+    else:
+        print("Então case!!")
+else:
+    quer_engordar = input("Você quer engordar ? s/n" )
+    if quer_engordar == "s":
+        print("Então case!!")
+    else:
+        print("Compre uma bicicleta")
+               
+
+
