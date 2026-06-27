f659c64090e22ed950b32a0b452865af2fa5b2a3
Samimiss <samilyyohana@gmail.com>
2026-06-26 21:26:07 -0300

    fodastico!!

---
+++ Vetores.py
@@ -0,0 +1,13 @@
+# Atividade de vetores
+
+lista=["maçã","banana",'abacaxi','cereja','ameixa','mamão','abacate','uva','goiaba','cacau','jaca','anga','jamelão','melancia','melão']
+
+for i in range (15):
+    print(lista[i])
+
+
+# print(lista[-9])
+# ("\n lista".join(lista))
+# len= conta quantas frutas tem na lista
+# lista.sort = ordem alfabetica
+#(f"{i+1}° lista {lista [i]}")= põe numeros antes dos nomes
